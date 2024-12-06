1cccca726719089daeae562746985d34c0a33b5a
angelo-004 <shoyothegreat04@gmail.com>
2024-12-06 22:25:44 +0800

    back to main

--- main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Project(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    image = models.ImageField(upload_to='img/projects/')
-
-    def __str__(self):
-        return self.title
